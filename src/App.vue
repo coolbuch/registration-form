69337979afe3490e0e14d4d4f59e73a0c58e402c
coolbuch <kovdima2000@gmail.com>
2023-02-08 12:49:27 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="purple" dark>
+      <v-app-bar-title>AAA</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <v-form>
+          <v-text-field title="Введите имя"></v-text-field>
+          <v-date-picker color="purple lighten-4"></v-date-picker>
+        </v-form>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
