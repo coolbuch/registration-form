08de4ddd5f673aa7b24d60190b588f4249551c44
coolbuch <kovdima2000@gmail.com>
2023-02-08 13:56:50 +0800

    add datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app>
     <v-app-bar app color="purple" dark>
-      <v-app-bar-title>AAA</v-app-bar-title>
+      <v-app-bar-title>Titile</v-app-bar-title>
     </v-app-bar>
     <v-main>
       <v-container fluid>
-        <v-form>
-          <v-text-field title="Введите имя"></v-text-field>
-          <v-date-picker color="purple lighten-4"></v-date-picker>
+        <v-form >
+          <v-text-field label="Введите имя"></v-text-field>
+          <v-text-field label="Введите фамилию"></v-text-field>
+          <v-text-field label="Введите отчество"></v-text-field>
+          <my-date-picker label="Дата рождения"></my-date-picker>
+          <my-radio-group v-model="answer" :items="['М', 'Ж']"></my-radio-group>
         </v-form>
       </v-container>
     </v-main>
@@ -15,12 +18,15 @@
 </template>
 
 <script>
+import MyDatePicker from "@/components/UI/MyDatePicker.vue"
+import MyRadioGroup from "@/components/UI/MyRadioGroup.vue"
 export default {
   name: "App",
 
-  components: {},
+  components: {MyDatePicker, MyRadioGroup},
 
   data: () => ({
+    radio,
     //
   }),
 };
---
+++ src/components/UI/MyRadioGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-radio-group v-model="answer">
+            <v-radio 
+                v-for="item in setItemsId" 
+                :key="item.id" 
+                :label="item.item"
+                :value="item.id"
+                @click="returnAnswer">
+            </v-radio>
+    </v-radio-group>
+</template>
+
+<script>
+export default {
+    props:{
+        items: []
+    },
+    data: () =>({
+        elements: [],
+        asnwer,
+    }),
+    methods:{
+        createElement(id, item){
+            return {id, item};
+        },
+        returnAnswer(){
+            this.$emit('answer', asnwer);
+        }
+    },
+    computed:{
+        setItemsId: function (){
+            let counter = 0;
+            this.items.forEach(element => {
+                this.elements.push(this.createElement(counter, element))
+                counter++;
+            });
+            return this.elements;
+        }
+    }
+}
+</script>
