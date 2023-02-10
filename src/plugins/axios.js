a5f77ea35481c5d902a57cd84205377baa4c87b0
coolbuch <kovdima2000@gmail.com>
2023-02-10 16:42:31 +0800

    changed url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://192.168.0.103:8000/api',
+  //http://192.168.0.103:8000/api
+  
+  baseURL: 'https://53e6d1a2-e1b5-4d74-bcce-42450a1544b7.mock.pstmn.io',
   
 });
